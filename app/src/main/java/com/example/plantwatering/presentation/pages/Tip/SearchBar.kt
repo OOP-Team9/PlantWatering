3d5c2917c4e2c7f6aeba25dbdb35fd23dc99eb2f
yaaan7 <wangpajeon@gmail.com>
2025-11-19 18:23:20 +0900

    파일 구조 수정

---
+++ app/src/main/java/com/example/plantwatering/presentation/pages/Tip/SearchBar.kt
@@ -0,0 +1,19 @@
+package com.example.plantwatering.presentation.pages.Tip
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TipScreen(){
+    Box(modifier = Modifier
+        .fillMaxSize(),
+        contentAlignment = Alignment.Center){
+        Text(text = "TipScreen",
+            fontSize = 22.sp)
+    }
+}
