11abd355283c80199d6ae3fc8d771f5c72e2a3a9
chxx1902 <cgim1902@gmail.com>
2025-11-20 16:20:19 +0900

    start

---
+++ app/src/main/java/com/example/plantcare/presentation/register/RegisterViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.plantcare.presentation.register
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class RegisterViewModel : ViewModel() {
+
+    private val _plantName = MutableStateFlow("")
+    val plantName: StateFlow<String> = _plantName
+
+    private val _wateringInterval = MutableStateFlow("")
+    val wateringInterval: StateFlow<String> = _wateringInterval
+
+    private val _lastWaterDate = MutableStateFlow("")
+    val lastWaterDate: StateFlow<String> = _lastWaterDate
+
+    fun updateName(new: String) {
+        _plantName.value = new
+    }
+
+    fun updateInterval(new: String) {
+        _wateringInterval.value = new
+    }
+
+    fun updateLastDate(new: String) {
+        _lastWaterDate.value = new
+    }
+}
+
