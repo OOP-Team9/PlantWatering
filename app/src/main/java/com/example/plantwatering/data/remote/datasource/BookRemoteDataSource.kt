37af2a8ac4fb4c2c9a6d31086659a06cce26b453
qkwltkwkd1 <wldndi2@kau.kr>
2025-12-17 11:52:46 +0900

    수정

--- app/src/main/java/com/example/plantwatering/data/remote/datasource/BookRemoteDataSource.kt
+++ app/src/main/java/com/example/plantwatering/data/remote/datasource/BookRemoteDataSource.kt
@@ -3,7 +3,8 @@ package com.example.plantwatering.data.remote.datasource
 import com.example.plantwatering.data.remote.dto.BookDto
 
 interface BookRemoteDataSource {
-    suspend fun getBooks(): List<BookDto>
-    suspend fun getBook(bookId: String): BookDto?
+    suspend fun getBooks(): List<BookDto> //도감 목록 전체
+    //비동기
+    suspend fun getBook(bookId: String): BookDto? //1개
 }
 
